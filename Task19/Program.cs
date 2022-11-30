a91010e7e027ccd8095c5aec48bafd02d41de899
AsyaKovach <asyakovach1996@gmail.com>
2022-11-30 19:44:50 +0300

    sun

---
+++ Task19/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом. Через строку решать нельзя.
+
+Console.WriteLine("Введите пятизначное число:");
+int number = int.Parse(Console.ReadLine()!);
+CheckPalindromicNumber(number);
+
+void CheckPalindromicNumber(int number)
+{
+    if (number >= 10000)
+    {
+        int num1 = (number / 10000) % 10;
+        int reg1 = number % 10;
+
+        if (num1 == reg1)
+        {
+            number = number / 10;
+            int num2 = (number / 100) % 10;
+            int reg2 = number % 10;
+            if (num2 == reg2)
+            {
+                Console.WriteLine("Является полиндромом");
+            }
+        }
+        else
+            Console.WriteLine("Не является полиндромом");
+    }
+}
---
+++ Task21/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+Console.WriteLine("Введите координату X первой точки");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату Y первой точки");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату D первой точки");
+int d1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату D второй точки");
+int d2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату X второй точки");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату Y второй точки");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+int G = x2 - x1;
+int B = y2 - y1;
+int O = d1 - d2;
+
+double length = Math.Sqrt(G * G + B * B + O * O);
+Console.WriteLine($"Длинна {length}");
