aba59f7653bf9191fe0f6792b507632644e48896
AsyaKovach <asyakovach1996@gmail.com>
2022-12-09 02:44:48 +0300

    Task29

---
+++ Task29/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов с клавиатуры и выводит массив на экран.
+Console.Write($"Размерность массива:  ");
+int num = Convert.ToInt32(Console.ReadLine());
+int size =8;
+int [] array = new int[size];
+
+Massiv(num);
+
+int[] Massiv(int arg)
+{
+for (int i = 0; i < 8; i++)
+{
+   arg= new Random().Next(1, 11);
+   Console.Write($"{arg} ");
+}
+return arg;
+}
