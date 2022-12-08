827291a463cf7c41aaf8f073fc71e9ba3495c3c4
AsyaKovach <asyakovach1996@gmail.com>
2022-12-09 02:46:15 +0300

    Task30

---
+++ Task30/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 30: - HARD необязательная Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.. Далее надо посчитать количество нулей и единиц, 
+//если единиц больше чем нулей то вывести TRUE на экран, иначе вывести False.
+
+int[] arg1 = new int[8];
+int count =0;
+int count1=0;
+Zero(arg1);
+One(arg1);
+
+int [] Zero(int [] arg)
+{
+for (int i = 0; i < 8; i++)
+{
+    arg[i]= new Random().Next(0, 2);
+    Console.WriteLine($"Элементы массива:  {arg[i]}");
+}
+return arg;
+}
+
+int [] One(int [] a)
+{
+for (int i = 0; i < 8; i++)
+if(a[i]==0)
+{
+    count++;
+}
+else
+{
+    count1++;
+}
+if (count1>count)
+{
+   Console.WriteLine($"TRUE");
+}
+else
+{
+  Console.WriteLine($"False");
+}
+return a;
+}
