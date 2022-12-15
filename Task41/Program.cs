6415d1f9bf7d8dc6259e12f6c9dee0835650db4c
AsyaKovach <asyakovach1996@gmail.com>
2022-12-15 23:16:35 +0300

    Task41

---
+++ Task41/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Write("Укажите колличество чисел: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+ManyNambers(num);
+Console.WriteLine($"Пользователь ввел {count} положительных чисел");
+int ManyNambers(int k)
+{
+    for (int i = 1; i <= k; i++)
+    {
+        Console.Write($"Введите число {i}:  ");
+        int num = Convert.ToInt32(Console.ReadLine());
+        if (num > 0) count++;
+        Console.WriteLine();
+    }
+    return count;
+}
