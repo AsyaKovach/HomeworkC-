0b27f0b2a652d5196d8b6ae06b5d736f96da30fa
AsyaKovach <asyakovach1996@gmail.com>
2022-12-23 20:30:55 +0300

    Task64

--- Task64/Program.cs
+++ Task64/Program.cs
@@ -3,13 +3,13 @@
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
-Console.Write("Введите целое число N: "); 
-int N= Convert.ToInt32(Console.ReadLine()); 
- 
-Console.WriteLine($" N={N} -> {Posled (N)}"); 
- 
-string Posled(int N) 
-{ 
-    if (N == 1) return Convert.ToString(N); 
-    else return Convert.ToString(N)  + ", " + Posled(N - 1); 
+Console.Write("Введите целое число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($" N={N} -> {Posled(N)}");
+
+string Posled(int N)
+{
+    if (N == 1) return Convert.ToString(N);
+    else return Convert.ToString(N) + ", " + Posled(N - 1);
 }
