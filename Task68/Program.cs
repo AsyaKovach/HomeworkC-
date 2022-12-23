e06effc322bbcb6135ef690bb71a43782529db08
AsyaKovach <asyakovach1996@gmail.com>
2022-12-23 20:31:42 +0300

    Task68

--- Task68/Program.cs
+++ Task68/Program.cs
@@ -2,20 +2,20 @@
 // m = 2, n = 3 -> A(m,n) = 9
 // m = 3, n = 2 -> A(m,n) = 29
 
-Console.Write("Введите целое число M: "); 
-int N = Convert.ToInt32(Console.ReadLine()); 
-Console.Write("Введите целое число N: "); 
+Console.Write("Введите целое число M: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите целое число N: ");
 int M = Convert.ToInt32(Console.ReadLine());
 
-Console.Write($"{A(N,M)}");
+Console.Write($"{A(N, M)}");
 
 static int A(int n, int m)
 {
-  if (n == 0)
-    return m + 1;
-  else
-    if ((n != 0) && (m == 0))
-      return A(n - 1, 1);
+    if (n == 0)
+        return m + 1;
     else
-      return A(n - 1, A(n, m - 1));
+      if ((n != 0) && (m == 0))
+        return A(n - 1, 1);
+    else
+        return A(n - 1, A(n, m - 1));
 }
