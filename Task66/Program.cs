90df47d3a413cd7f4e2b8265fc4778193339ef71
AsyaKovach <asyakovach1996@gmail.com>
2022-12-23 20:31:20 +0300

    Task66

--- Task66/Program.cs
+++ Task66/Program.cs
@@ -2,16 +2,16 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
-Console.Write("Введите целое число M: "); 
-int M = Convert.ToInt32(Console.ReadLine()); 
-Console.Write("Введите целое число N: "); 
-int N = Convert.ToInt32(Console.ReadLine()); 
-int sum =0;
+Console.Write("Введите целое число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите целое число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
 
-Console.WriteLine($"M={M}; N={N} -> {SumMN(N,M)}"); 
- 
-int SumMN (int N, int M) 
-{ 
-    if (N == M) return M; 
-    else return sum = SumMN(N - 1, M) + N; 
+Console.WriteLine($"M={M}; N={N} -> {SumMN(N, M)}");
+
+int SumMN(int N, int M)
+{
+    if (N == M) return M;
+    else return sum = SumMN(N - 1, M) + N;
 }
